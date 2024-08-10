5b5361e20cc5185ecba281ef2578db2bfc2cfbdd
SaihanCR <saihancastillo83@gmail.com>
2024-08-09 23:58:16 -0400

    Creacion del DAO Contacto

--- src/com/itla/db/DBManager.java
+++ src/com/itla/db/DBManager.java
@@ -1,5 +1,5 @@
 package com.itla.db;
 
 public class DBManager {
-    
+
 }
--- src/com/itla/modelo/Contacto.java
+++ src/com/itla/modelo/Contacto.java
@@ -1,5 +1,74 @@
 package com.itla.modelo;
 
 public class Contacto {
-    
+        
+        private Long idEmpleado = null;
+        
+        private String nombre;
+        
+        private String apellidos;
+        
+        private String empresa;
+        
+        private String telefono;
+        
+        private String correo;
+
+    public Contacto(String nombre, String apellidos, String empresa, String telefono, String correo) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.empresa = empresa;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public Long getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Long idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+        
+        
 }
